d9631126bb9703a898d158ca9c01938d44174746
re0ah <re0ah@yandex.ru>
2021-11-10 03:33:47 +0300

    к полям ввода редактора добавлен счетчик строк. Недопилен до конца

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,4 +1,3 @@
-try {
 {
 	const initCheckbox2 = (obj_name, checkbox_name, disp) => {
 		const func = () => document.getElementById(obj_name).style.display = document.getElementById(checkbox_name).checked ? disp : "none";
@@ -17,6 +16,9 @@ const changeEditorHandler = function() {
 	let value_html = $("#content_html").value
 	let value_css = $("#content_css").value
 	$("#content_view").innerHTML = `<style>${value_css}</style>${value_html}`;
+	($("#content_html"), $("#lines_html"));
+	updateLines($("#content_css"), $("#lines_css"));
+	updateLines($("#content_js"), $("#lines_js"));
 }
 
 $("#content_html").addEventListener("input", changeEditorHandler);
@@ -59,90 +61,42 @@ function initFontInput(inputId) {
 	let el = $(`#${inputId}`);
 	el.value = getCookie(inputId);
 	$(`#${el.name}`).style.fontSize = `${el.value}px`;
+	document.documentElement.style.setProperty(el.lang, `${el.value}px`);
 	el.addEventListener("input", function () {
 		document.cookie = `${this.id}=${this.value}`;
 		$(`#${this.name}`).style.fontSize = `${this.value}px`;
+		document.documentElement.style.setProperty(this.lang, `${this.value}px`);
 	})
 }
 initFontInput("html_font_size");
 initFontInput("css_font_size");
 initFontInput("js_font_size");
 
-
-}
-catch
-{
+function updateLines(textarea, lines) {
+	const line_count = textarea.value.split("\n").length;
+	const child_count = lines.children.length;
+	let diff = line_count - child_count;
+	if(diff > 0) {
+		const fragment = document.createDocumentFragment();
+		for (let i = diff; i > 0; i--) {
+			const line_number = document.createElement("span");
+			line_number.className = `text-area-line ${textarea.name}`;
+			fragment.appendChild(line_number);
+		}
+		lines.appendChild(fragment);
+	}
+	else {
+		for (let i = diff; i < 0; i++) {
+			lines.removeChild(lines.lastChild);
+		}
+	}
 }
 
 function initTextAreaLines(textarea, lines) {
-	function updateLines() {
-		const line_count = textarea.value.split("\n").length;
-		const child_count = lines.children.length;
-		let diff = line_count - child_count;
-		if(diff > 0) {
-			const fragment = document.createDocumentFragment();
-			for (let i = diff; i > 0; i--) {
-				const line_number = document.createElement("span");
-				line_number.className = "text-area-line";
-				fragment.appendChild(line_number);
-			}
-			lines.appendChild(fragment);
-		}
-		else {
-			for (let i = diff; i < 0; i++) {
-				lines.removeChild(lines.lastChild);
-			}
-		}
-	}
-
 	updateLines(textarea, lines);
 
 	const changeHandler = ((textarea, lines) => () => {
 		updateLines(textarea, lines);
-		console.log("change");
-	})(textarea, lines);
-	textarea.addEventListener("input", changeHandler);
-
-	const scrollHandler = ((textarea, lines) => () => {
-		lines.scrollTop = textarea.scrollTop;
-		console.log("scroll");
-	})(textarea, lines);
-	const scrollEvents = ["change", "mousewheel", "scroll"];
-	[...scrollEvents].forEach((scrollEvent) => {
-		textarea.addEventListener(scrollEvent, scrollHandler);
-	});
-}
-
-//initTextAreaLines($("#content_html"), $("#lines_html"));
-//initTextAreaLines($("#content_css"), $("#lines_css"));
-//initTextAreaLines($("#content_js"), $("#lines_js"));
-
-
-function initLines(textarea, lines) {
-	function updateLineNumbers(textarea, lines) {
-		const lineCount = textarea.value.split("\n").length;
-		const childCount = lines.children.length;
-		let diff = lineCount - childCount;
-		if(diff > 0) {
-			const fragment = document.createDocumentFragment();
-			for (let i = diff; i > 0; i--) {
-				const lineNumber = document.createElement("span");
-				lineNumber.className = "tln-line";
-				fragment.appendChild(lineNumber);
-			}
-			lines.appendChild(fragment);
-		}
-		else {
-			for (let i = diff; i < 0; i++) {
-				lines.removeChild(lines.lastChild);
-			}
-		}
-	}
-
-	updateLineNumbers(textarea, lines);
-
-	const changeHandler = ((textarea, lines) => () => {
-		updateLineNumbers(textarea, lines);
 	})(textarea, lines);
 	textarea.addEventListener("input", changeHandler);
 
@@ -155,7 +109,6 @@ function initLines(textarea, lines) {
 	});
 }
 
-console.log(document.getElementById("textarea_1"));
-initLines(document.getElementById("textarea_1"), document.getElementById("TLN_LEFT1"));
-initLines(document.getElementById("textarea_2"), document.getElementById("TLN_LEFT2"));
-initLines(document.getElementById("textarea_3"), document.getElementById("TLN_LEFT3"));
+initTextAreaLines($("#content_html"), $("#lines_html"));
+initTextAreaLines($("#content_css"), $("#lines_css"));
+initTextAreaLines($("#content_js"), $("#lines_js"));
